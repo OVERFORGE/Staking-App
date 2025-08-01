89b6ec83631f5517f59498685cce3660fab9dc53
OVERFORGE <dkdakshkhurana@gmail.com>
2025-08-02 04:21:52 +0530

    Git fix

--- Components/PoolsModel.jsx
+++ Components/PoolsModel.jsx
@@ -1,7 +1,56 @@
-import React from "react";
-
-const PoolsModel = () => {
-  return <div>PoolsModel</div>;
+import React, { useState } from "react";
+import { IoMdClose } from "./ReactICON";
+import PopUpInputField from "./Admin/RegularComp/PopUpInputField";
+import PupUpButton from "./Admin/RegularComp/PupUpButton";
+import InputRatio from "./Admin/RegularComp/InputRatio";
+const PoolsModel = ({
+  deposit,
+  poolID,
+  address,
+  selectedPool,
+  selectedToken,
+  setLoader,
+}) => {
+  const [amount, setAmount] = useState();
+  const CALLING_FUNCTION = async (poolID, amount, address) => {
+    setLoader(true);
+    console.log(pool);
+    const receipt = await deposit(poolID, amount, address);
+    if (receipt) {
+      console.log(receipt);
+      setLoader(false);
+      window.location.reload();
+    }
+    setLoader(false);
+  };
+  return (
+    <div
+      className="modal modal--auto fade"
+      id="modal-apool"
+      tabIndex={-1}
+      aria-labelledby="modal-apool"
+      aria-hidden="true"
+    >
+      <div className="modal-dialog modal-dialog-centered">
+        <div className="modal-content">
+          <div className="modal__content">
+            <button
+              className="modal__close"
+              type="button"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            >
+              <i className="ti ti-x">
+                <IoMdClose />
+              </i>
+            </button>
+            <h4 className="modal__title">Invest</h4>
+            <p className="modal__text">Welcome to Stoken</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default PoolsModel;
