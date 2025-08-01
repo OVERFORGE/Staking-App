6e78fcb7ef3e033afc96f548c2b1e301bb609aa5
OVERFORGE <dkdakshkhurana@gmail.com>
2025-08-02 03:20:48 +0530

    Token module completed

--- Components/Token.jsx
+++ Components/Token.jsx
@@ -1,7 +1,92 @@
 import React from "react";
 
-const Token = () => {
-  return <div>Token</div>;
+const Token = ({ poolDetails }) => {
+  return (
+    <div className="section">
+      <div className="container">
+        <div className="row">
+          <div className="col-12 col-md-10 offset-md-1 col-lg-6 offset-lg-3">
+            <div className="section__title">
+              <h2>Token</h2>
+              <p>More features with own Token.</p>
+            </div>
+          </div>
+        </div>
+        <div className="row row--relative">
+          <div className="col-12">
+            <div className="invest invest--big">
+              <h2 className="invest__title">
+                <div className="row">
+                  <div className="col-12 col-lg-5">
+                    <div className="invest__rate-wrap">
+                      <div className="invest__rate">
+                        <span>Stake Supply</span>
+                        <p>
+                          {poolDetails?.depositToken?.totalSupply}{" "}
+                          {poolDetails?.depositToken?.symbol}{" "}
+                        </p>
+                      </div>
+                      <div className="invest__green">
+                        <img src="img/graph/graph2.svg" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 col-lg-5 offset-lg-1">
+                    <div className="invest__rate-wrap">
+                      <div className="invest__rate">
+                        <span>Total Stake</span>
+                        <p className="green">
+                          {poolDetails?.depositToken?.contractTokenBalance.slice(
+                            0,
+                            10
+                          )}{" "}
+                          {poolDetails?.depositToken?.symbol}
+                        </p>
+                      </div>
+                      <div className="invest__graph">
+                        <img src="img/graph/graph1.svg" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 col-lg-5 ">
+                    <div className="invest__rate-wrap">
+                      <div className="invest__rate">
+                        <span>Reward Token</span>
+                        <p className="red">
+                          {poolDetails?.rewardToken.totalSupply}{" "}
+                          {poolDetails?.rewardToken.symbol}
+                        </p>
+                      </div>
+                      <div className="invest__graph">
+                        <img src="img/graph/graph3.svg" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-12 col-lg-5 offset-lg-1">
+                    <div className="invest__rate-wrap">
+                      <div className="invest__rate">
+                        <span>Total Stake</span>
+                        <p className="green">1 ExE = 0.00001 ETH</p>
+                      </div>
+                      <div className="invest__graph">
+                        <img src="img/graph/graph4.svg" alt="" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <p className="invest__info">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae
+                  obcaecati magnam quis dolorem illo itaque, nobis quas ullam
+                  adipisci dolor debitis labore facere veniam possimus optio
+                  cumque perspiciatis. Expedita, harum.
+                </p>
+              </h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Token;
